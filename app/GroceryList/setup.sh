763308a0bd25ce99b396c7e8120f84c9ff62a0f4
Isara Aomi <isara.aomi.87u@gmail.com>
2024-12-27 22:46:36 +0900

    Added setup files.

---
+++ app/GroceryList/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#--- Alias
+alias la='ls -la'
+alias ..='cd ..'
+alias ...='cd ..; cd ..'
+alias cd..='cd ..'
+alias cd...='cd ..; cd ..'
+alias cd-='cd -'
+alias df='df -h'
+alias rm='rm -v'
+alias cp='cp -v'
+alias mv='mv -v'
+alias du='du -h'
+alias cls='clear'
+alias tar_czvfp='tar czvfp'
+alias tar_xzvfp='tar xzvfp'
